3622d4ce5f169e482c14a5fc84ad830908c46ffc
iechenyb <zzuchenyb@sina.com>
2017-10-22 22:14:23 +0800

    集群session共享

--- src/main/java/com/cyb/controller/RequestController.java
+++ src/main/java/com/cyb/controller/RequestController.java
@@ -9,20 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cyb.po.User;
 import com.cyb.utils.RequestUtils;
-
+/**
+ * 
+    http://localhost:8088/mvc-2/infor.do
+ */
 @RestController("req")
 public class RequestController {
+	
 	@RequestMapping("/infor")
 	public String infor(HttpServletRequest request) {
 		StringBuffer sb = new StringBuffer();
 		sb.append("romoteIp:" + RequestUtils.getRemoteHost(request) + ",");
 		sb.append("is ajax request:" + RequestUtils.isAjaxRequest(request) + ",");
-		sb.append("is req from mobile:" + RequestUtils.JudgeIsMoblie(request));
+		sb.append("is req from mobile:" + RequestUtils.JudgeIsMoblie(request)+",");
+		sb.append("sessionId:"+request.getSession().getId()+",");
+		sb.append("username:"+request.getSession().getAttribute("userName")+",");
+		System.out.println("请求信息："+sb.toString());
 		return sb.toString();
 	}
-
+	
+	@RequestMapping("/login")
+	public String login(HttpServletRequest request,String name) {
+		request.getSession().setAttribute("userName", name);
+		return "login success ,your name is "+name;
+	}
 	@ResponseBody // 返回string的时候，必须加 @responseBody注解
-	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
 	public User get(String id) {
 		User user = new User();
 		user.setName(id);
